adfa58bf34b13ce8cb3b20f1cc26b12fd46ee3db
LittleAngel4ever <Minsk_x250@tutamail.com>
2025-05-23 11:50:32 +0300

    //

--- exception.py
+++ exception.py
@@ -6,31 +6,36 @@ class DataFrameStructureError(Exception):
 class DataFrameTypeError(Exception):
     pass
 
+# Handling errors
 class DataFrame:
     def __init__(self, filename):
         try:
             self.df = pd.read_csv(filename)
-        except FileNotFoundError:
-            print("No such file")
-        except pd.errors.DataError:
-            print("No data in file")
-        except pd.errors.ParserError:
-            print("Wrong data")
+        except FileNotFoundError: # File presence error
+            print('The following error occurred: No such file of directory: ', filename)
+            raise SystemExit()
+        except pd.errors.EmptyDataError: # Error in the presence of data in the file
+            print("The following error occurred: Dataframe is empty.")
+            raise SystemExit()
+        except pd.errors.ParserError: # Strange Column Placement Error
+            print("The following error occurred: Strange column arrangement")
+            raise SystemExit()
         try:
             self.column_list_from_file = self.df.columns.to_list()
-            self.df_original = pd.read_csv("var7original.csv")
-            self.column_list_main = self.df_original.columns.to_list()
+            self.df_orig = pd.read_csv("var7original.csv")
+            self.column_list_main = self.df_orig.columns.to_list()
             if self.column_list_from_file != self.column_list_main:
                 raise DataFrameStructureError("Wrong structure")
-        except DataFrameStructureError:        
+        except DataFrameStructureError: # Dataframe structure error
             print("Please, remake your columns, we were waiting for", self.column_list_main, ", but you given", self.column_list_from_file)
+            raise SystemExit()
         try:
             self.df_input_types = str(self.df.dtypes)
-            self.df_original_types = str(self.df_original.dtypes)
+            self.df_original_types = str(self.df_orig.dtypes)
             if self.df_input_types != self.df_original_types:
                 raise DataFrameTypeError("Wrong types")
             else:
-                print("Everything alright")
+                print("Reading the dataframe completed successfully.")
         except:
             print("Wrong types\n" 
             "Expected: ", self.df_original_types,
